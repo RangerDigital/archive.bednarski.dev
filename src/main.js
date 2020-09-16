629d7d57bc9f62e2a070096680d8818e423cdd42
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-09-16 10:31:13 +0200

    Add Gtag

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from "./App.vue";
 import "fullpage.js/vendors/scrolloverflow";
 import VueFullPage from "vue-fullpage.js";
 import anime from "animejs/lib/anime.min.js";
+import VueGtag from "vue-gtag";
 
 Vue.prototype.$anime = anime;
+
+Vue.use(VueGtag, {
+  config: { id: "UA-177948119-1" },
+});
 
 Vue.use(VueFullPage);
 Vue.config.productionTip = false;
